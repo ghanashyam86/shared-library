ebe2ecddd8da34b08f6637473c281c77f4fe1754
sham <sham@gmail.com>
2026-03-25 04:56:44 +0000

    buildApp file committing

---
+++ vars/buildApp.groovy
@@ -0,0 +1,13 @@
+def call() {
+   echo "===================================="
+   echo "        Starting build stage        "
+   echo "==================================="
+
+   echo "Wring first shared libroy"
+   echo " Its easy to learn"
+   echo "it is very easy "
+   echo "I will learn it very fast"
+
+   echo "=================================="
+   echo "      Build stage complet         "
+   echo "=================================="
