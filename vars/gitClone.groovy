6b84b649df85850787401235fd8a95ef6cf0888d
sham <sham@gmail.com>
2026-03-26 10:00:14 +0530

    new

--- vars/gitClone.groovy
+++ vars/gitClone.groovy
@@ -7,3 +7,21 @@ def call(String repoUrl) {
      sh "git clone ${repoUrl}"
 }
 }
+
+
+def call(Map args) {
+   if (!args.repo) {
+      error "Missing required parameter: repo"
+
+      def target = args.target ?: "/mnt/project"
+      def branch = args.branch ?: "main"
+
+      echo "Cloning repository: ${args.repo}"
+      echo "Branch: ${branch}"
+      echo "Target Directory: ${target}"
+
+     dir(target) {
+       sh "rm -rf *"
+       sh "git clone -b ${branch} ${args.repo} ."
+  }
+}
