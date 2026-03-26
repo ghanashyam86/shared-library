e5c59862a464d74118d666212bead05828f987f4
sham <sham@gmail.com>
2026-03-26 09:42:38 +0530

    new

--- vars/printInfo.groovy
+++ vars/printInfo.groovy
@@ -1,7 +1,7 @@
 
 def call(Map args) {
   def userName = args.name ?: "Guest"
-  def environment = args.env ?: "qa"
+  def environment = args.env ?: "uat"
 
   echo "User: ${userName}"
   echo "Environment: ${environment}"
